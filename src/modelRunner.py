cc46a298063992dd5c27aab17bb5531acfbacd85
Ali Tourani <a.tourani1991@gmail.com>
2023-10-12 13:02:59 +0200

    [feat] - improve implementation

--- src/modelRunner.py
+++ src/modelRunner.py
@@ -1,4 +1,7 @@
-from .fastsam import FastSAM
+import torch
+from .fastsam import FastSAM, FastSAMPrompt
+
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
 def fastSamInit(name: str):
@@ -13,3 +16,34 @@ def fastSamInit(name: str):
     # Initialization
     model = FastSAM(name)
     return model
+
+
+def fastSamSegmenter(image, model):
+    """
+    Segments the given image using Fast SAM
+
+    Parameters
+    -------
+    image: Mat
+        The input image for segmentation
+    model: dict
+        A registered model of Fast SAM
+
+    Returns
+    -------
+    masks: dict
+        The masks created by the model containing segments
+    """
+    print()
+    # Generating mask (everything result)
+    # maskGenerator = model(
+    #     image,
+    #     device=DEVICE,
+    #     retina_masks=True,
+    #     imgsz=fastSam_imageSize,
+    #     conf=fastSam_conf,
+    #     iou=fastSam_iou
+    # )
+    # # Process
+    # masks = FastSAMPrompt(image, maskGenerator, device=DEVICE)
+    # return masks
--- src/segmenter.py
+++ src/segmenter.py
@@ -5,20 +5,35 @@ import cv2 as cv
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from utils.helpers import cleanMemory, monitorParams
 
 
 class Segmenter:
     def __init__(self):
+        # Initial checks
+        monitorParams()
+        cleanMemory()
+
         # Get parameters
-        print('Loading config parameters ...')
+        print('Loading configuration parameters ...')
         params = rospy.get_param('~params')
-        modelName = params['modelName']
+        modelName = params['model_params']['model_name']
+        modelPath = params['model_params']['model_path']
 
         # Subscribers
         # rospy.Subscriber(pose_topic, PoseStamped, self.sub_pose)
 
         # Publishers
         # publisherMask = rospy.Publisher('result_mask', Image, queue_size=10)
+
+        # Read input images
+        # image = cv.imread(imagePath)
+        # image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+        # # Run Segmentation
+        # fsam = fastSamInit()
+        # masks = fastSamSegmenter(image, fsam)
+        # fastSamShowOutput(masks)
 
 
 # Run the program
