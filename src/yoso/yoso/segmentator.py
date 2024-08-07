[fix] - reduced yoso temperature scaling

src/yoso/yoso/segmentator.py
@@ -177,7 +177,7 @@ class YOSO(nn.Module):
         mask_cls = F.softmax(mask_cls, dim=-1)[..., :-1]
         mask_pred = mask_pred.sigmoid()
         semseg = torch.einsum("qc,qhw->chw", mask_cls, mask_pred)
-        semseg = semseg/0.25  # temperature scaling
+        semseg = semseg/0.15  # temperature scaling
         semseg = F.softmax(semseg, dim=0) # probabilities
         return semseg
 