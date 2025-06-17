32fa44845619f4cb4899c17c43e751b6b92c6469
Ali Tourani <a.tourani1991@gmail.com>
2025-06-17 13:51:14 +0200

    [fix] - remove unused segmenters

--- src/fastsam/__init__.py
+++
@@ -1,8 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from .model import FastSAM
-from .prompt import FastSAMPrompt
-from .decoder import FastSAMDecoder
-from .predict import FastSAMPredictor
-
-__all__ = 'FastSAMPredictor', 'FastSAM', 'FastSAMPrompt', 'FastSAMDecoder'
