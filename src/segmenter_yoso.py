d5db637d7417eacea9caac47b561c29160936d2c
shaheer34mts <muhammad_shaheer_ahmed@outlook.com>
2026-01-16 09:06:32 +0000

    [Feat] - add reasoning msgs dependency

--- src/segmenter_yoso.py
+++ src/segmenter_yoso.py
@@ -10,7 +10,8 @@ from utils.helpers import cleanMemory, monitorParams
 from output import yosoVisualizer, entropyVisualizer
 from utils.semantic_utils import probabilities2ROSMsg
 from ament_index_python import get_package_share_directory
-from segmenter_ros.msg import SegmenterDataMsg, VSGraphDataMsg
+from segmenter_ros.msg import SegmenterDataMsg
+from situational_graphs_reasoning_msgs.msg import GraphKeyframes, Keyframe
 
 
 class Segmenter(Node):
@@ -28,10 +29,16 @@ class Segmenter(Node):
         cleanMemory()
 
         # Configuration parameters
-        ground_ids = self.get_parameter(
-            "params.output.classes.ground").get_parameter_value().integer_array_value.tolist()
-        wall_ids = self.get_parameter(
-            "params.output.classes.wall").get_parameter_value().integer_array_value.tolist()
+        ground_ids = (
+            self.get_parameter("params.output.classes.ground")
+            .get_parameter_value()
+            .integer_array_value.tolist()
+        )
+        wall_ids = (
+            self.get_parameter("params.output.classes.wall")
+            .get_parameter_value()
+            .integer_array_value.tolist()
+        )
         self.classes = [ground_ids, wall_ids]
 
         self.conf = (
@@ -89,69 +96,81 @@ class Segmenter(Node):
         )
 
         # Subscribers (to vS-Graphs)
-        self.create_subscription(
-            VSGraphDataMsg, rawImageTopic, self.segmentation, 10)
+        self.create_subscription(GraphKeyframes, rawImageTopic, self.segmentation, 10)
 
         # Publishers (for vS-Graphs)
-        self.publisherSeg = self.create_publisher(
-            SegmenterDataMsg, segImageTopic, 10)
-        self.publisherSegVis = self.create_publisher(
-            Image, segImageVisTopic, 10)
+        self.publisherSeg = self.create_publisher(SegmenterDataMsg, segImageTopic, 10)
+        self.publisherSegVis = self.create_publisher(Image, segImageVisTopic, 10)
 
         # ROS Bridge
         self.bridge = CvBridge()
 
     def segmentation(self, imageMessage):
-        try:
-            # Parse the input data
-            key_frame_id = imageMessage.key_frame_id
-            key_frame_image = imageMessage.key_frame_image
-
-            # Convert the ROS Image message to a CV2 image
-            cvImage = self.bridge.imgmsg_to_cv2(key_frame_image, "bgr8")
-
-            # Processing
-            filteredSegments, filteredProbs = yosoSegmenter(
-                cvImage, self.model, self.classes
-            )
-            if self.visualize:
-                segmented_image = yosoVisualizer(
-                    cvImage, filteredSegments, self.cfg)
-            segmentedUncImage = entropyVisualizer(filteredSegments["sem_seg"])
-
-            # Convert to ROS message
-            pcdProbabilities = probabilities2ROSMsg(
-                filteredProbs, imageMessage.header.stamp, imageMessage.header.frame_id
-            )
-
-            # Create a header with the current time
-            header = Header()
-            now = self.get_clock().now().to_msg()
-            header.stamp = now
-            header.frame_id = imageMessage.header.frame_id
-
-            # Publish the processed image to vS-Graphs
-            segmenterData = SegmenterDataMsg()
-            segmenterData.header = header
-            segmenterData.key_frame_id = key_frame_id
-            if self.visualize:
-                segmenterData.segmented_image = self.bridge.cv2_to_imgmsg(
-                    segmented_image, "bgr8"
+        # Parse the input data
+        # Process all keyframes in the icoming message one by one
+        # check if the message is coming
+        self.get_logger().info("[Segmenter] Received keyframes for segmentation.")
+        for keyframe in imageMessage.keyframes:
+            try:
+                key_frame_id = keyframe.id
+                key_frame_image = keyframe.keyframe_image
+                is_segmented = keyframe.is_segmented
+                if is_segmented:
+                    self.get_logger().info(
+                        f"[Segmenter] Keyframe {key_frame_id} is already segmented. Skipping..."
+                    )
+                    continue
+                # If not segmented, process the keyframe
+                self.get_logger().info(
+                    f"[Segmenter] Processing keyframe {key_frame_id}..."
                 )
-            segmenterData.segmented_image_uncertainty = self.bridge.cv2_to_imgmsg(
-                segmentedUncImage, "bgr8"
-            )
-            segmenterData.segmented_image_probability = pcdProbabilities
-            self.publisherSeg.publish(segmenterData)
+                # Convert the ROS Image message to a CV2 image
+                cvImage = self.bridge.imgmsg_to_cv2(key_frame_image, "bgr8")
 
-            if self.visualize:
-                # Publish the processed image for visualization
-                visualizationImgMsg = segmenterData.segmented_image
-                visualizationImgMsg.header = header
-                self.publisherSegVis.publish(visualizationImgMsg)
+                # Processing
+                filteredSegments, filteredProbs = yosoSegmenter(
+                    cvImage, self.model, self.classes
+                )
+                self.get_logger().info(
+                    f"[Segmenter] FINISHED PROCESSING keyframe {key_frame_id}."
+                )
+                # if self.visualize:
+                #     segmented_image = yosoVisualizer(cvImage, filteredSegments, self.cfg)
+                # segmentedUncImage = entropyVisualizer(filteredSegments["sem_seg"])
 
-        except CvBridgeError as e:
-            self.get_logger().error(f"[Segmenter] CvBridge error: {e}")
+                # # Convert to ROS message
+                # pcdProbabilities = probabilities2ROSMsg(
+                #     filteredProbs, imageMessage.header.stamp, imageMessage.header.frame_id
+                # )
+
+                # # Create a header with the current time
+                # header = Header()
+                # now = self.get_clock().now().to_msg()
+                # header.stamp = now
+                # header.frame_id = imageMessage.header.frame_id
+
+                # # Publish the processed image to vS-Graphs
+                # segmenterData = SegmenterDataMsg()
+                # segmenterData.header = header
+                # segmenterData.key_frame_id = key_frame_id
+                # if self.visualize:
+                #     segmenterData.segmented_image = self.bridge.cv2_to_imgmsg(
+                #         segmented_image, "bgr8"
+                #     )
+                # segmenterData.segmented_image_uncertainty = self.bridge.cv2_to_imgmsg(
+                #     segmentedUncImage, "bgr8"
+                # )
+                # segmenterData.segmented_image_probability = pcdProbabilities
+                # self.publisherSeg.publish(segmenterData)
+
+                # if self.visualize:
+                #     # Publish the processed image for visualization
+                #     visualizationImgMsg = segmenterData.segmented_image
+                #     visualizationImgMsg.header = header
+                #     self.publisherSegVis.publish(visualizationImgMsg)
+
+            except CvBridgeError as e:
+                self.get_logger().error(f"[Segmenter] CvBridge error: {e}")
 
 
 def main(args=None):
@@ -163,11 +182,9 @@ def main(args=None):
         node = Segmenter()
         rclpy.spin(node)
     except KeyboardInterrupt:
-        node.get_logger().info(
-            '[Segmenter] Node interrupted by user! Exiting...')
+        node.get_logger().info("[Segmenter] Node interrupted by user! Exiting...")
     except Exception as e:
-        rclpy.logging.get_logger().error(
-            f'[Segmenter] Unhandled exception: {e}')
+        rclpy.logging.get_logger().error(f"[Segmenter] Unhandled exception: {e}")
     finally:
         if node is not None:
             node.destroy_node()
